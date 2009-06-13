22315938922eb9f0916b03e98450e1f904ee1a4a
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2009-06-13 18:51:26 +0000

    this version is currently under review at http://ftp-master.debian.org/

--- debian/menu.ex
+++ debian/menu.ex
@@ -1,2 +1,3 @@
-?package(fim):needs="X11|text|vc|wm" section="Applications/Graphics"\
-  title="fim" command="/usr/bin/fim"
+# man menufile, /usr/share/doc/menu/html to edit this. for now it is disabled.
+#?package(fim):needs="X11|text|vc|wm" section="Applications/Graphics"\
+#  title="fim" command="/usr/bin/fim"
