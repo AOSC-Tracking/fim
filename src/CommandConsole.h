2f7e204c1bbeb7ebf89247a923186ecd64b7a20c
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2011-06-17 23:17:27 +0000

    taking the common code subset in aa/sdl's resizing code in a separate routine.

--- src/CommandConsole.h
+++ src/CommandConsole.h
@@ -358,6 +358,8 @@ gcc version 3.3 20030304 (Apple Computer, Inc. build 1495)
 	private:
 	fim::string get_commands_reference(FimDocRefMode refmode=DefRefMode)const;
 	fim::string get_variables_reference(FimDocRefMode refmode=DefRefMode)const;
+	public:
+	fim_err_t resize(fim_coo_t w, fim_coo_t h);
 };
 }
 
