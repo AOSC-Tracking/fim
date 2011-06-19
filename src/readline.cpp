3da0dcde6508098b5b0d9c8ed3aa2868349993f4
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2011-06-19 14:50:26 +0000

    bugfix: readline hooks for option-furnished sdl specification did not activate.

--- src/readline.cpp
+++ src/readline.cpp
@@ -298,7 +298,8 @@ void initialize_readline (fim_bool_t with_no_display_device)
 	        rl_pre_input_hook=redisplay_hook;
 	}
 #if defined(FIM_WITH_LIBSDL) || defined(FIM_WITH_AALIB)
-	if( g_fim_output_device==FIM_DDN_INN_SDL 
+	//if( g_fim_output_device==FIM_DDN_INN_SDL 
+	if(g_fim_output_device.find(FIM_DDN_INN_SDL)==0
 		/* only useful to bypass X11-windowed aalib (but sadly, breaks plain aalib input)  */ 
 		/*|| g_fim_output_device==FIM_DDN_INN_AA */ 
 	)
