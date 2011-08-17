c8a79394a4ba066bf6b4ec0a157d815fa17b1b34
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2011-08-17 17:50:01 +0000

    a first interfacing to the GraphicsMagick library (for loading files).

---
+++ trunk/src/fim_types.h
@@ -0,0 +1,32 @@
+/* $LastChangedDate$ */
+/*
+ fim_types.h : Fim misc declarations
+
+ (c) 2011-2011 Michele Martone
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+/* This file will grow, as definitions will be moved there from fim.h */
+
+#ifndef FIM_TYPES_FIM_H
+#define FIM_TYPES_FIM_H
+
+typedef unsigned char fim_byte_t;	/* a type for bytes */
+
+/* we wait for variadic macros support in standard C++ */
+#define FIM_FPRINTF fprintf
+
+#endif
