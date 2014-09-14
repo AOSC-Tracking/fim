91f4f5a4043133792aca051eb724a901b1f46e54
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2014-09-14 19:04:40 +0000

    wrote a mouse-driven image panning functionality for sdl (non active by default). bugfix to Viewport::pan_left and Viewport::pan_right.

--- trunk/src/fim.h
+++ trunk/src/fim.h
@@ -118,6 +118,9 @@
 #define FIM_LINE_CERR std::cerr << "fatal error" << __FILE__ << ":" << __LINE__ << "\n";
 #define FIM_LINE_COUT std::cout << "in " <<__func__ << " # " << __FILE__ << ":" << __LINE__ << "\n";
 #define FIM_INT_PCNT(P,L) ((fim_int)ceilf((float)((P)*(L))/100.0)) //FIXME: gross errors may occur here
+#define FIM_INT_PCNT_SAFE(P,L) FIM_INT_PCNT(FIM_MIN(100,FIM_MAX(P,0)),(L))
+#define FIM_INT_DET_PCNT(P,T) ( (fim_int) ((((double)(P))/((double)(T)))*100.0) )
+#define FIM_DELIMIT_TO_100(X) FIM_MIN(FIM_MAX(X,0),100)
 
 #include "fim_types.h"
 #include "fim_limits.h"
