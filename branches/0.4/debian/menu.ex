442a2e74e4dbc9c11832c5ca65ac9cd7b1f4b1c0
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2015-01-21 13:48:44 +0000

    replaced the old debian/* files with the ones kindly provided by Gianfranco Costamagna.

--- branches/0.4/debian/menu.ex
+++
@@ -1,3 +0,0 @@
-# man menufile, /usr/share/doc/menu/html to edit this. for now it is disabled.
-#?package(fim):needs="X11|text|vc|wm" section="Applications/Graphics"\
-#  title="fim" command="/usr/bin/fim"
