eb65bbc68759ff29e9917e2dd679968c4e76e3c5
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2015-02-12 11:09:00 +0000

    simplifying the CommandConsole variable read/modify methods.

--- src/CommandConsole-var.cpp
+++ src/CommandConsole-var.cpp
@@ -23,6 +23,148 @@
 
 namespace fim
 {
+
+	fim::string CommandConsole::rnid(const fim::string & varname)const
+	{
+		fim::string id;
+#ifdef FIM_NAMESPACES
+		if( varname[1]==FIM_SYM_NAMESPACE_SEP && varname.length() > 2 )
+		{
+			if( varname.length() > 2 )
+				id = varname.c_str()+2;
+		}
+		else
+#endif /* FIM_NAMESPACES */
+			id = varname;
+		return id;
+	}
+
+	Namespace * CommandConsole::rns(const fim::string varname)
+	{
+		Namespace * nsp = NULL;
+#ifdef FIM_NAMESPACES
+		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
+		{
+			try
+			{
+			//a specific namespace was selected!
+			fim_char_t ns = varname[0];
+			fim::string id = varname.c_str()+2;
+
+			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
+#ifdef FIM_WINDOWS
+			{
+				//window variable
+				nsp = window_;
+				goto err;
+			}
+			else
+			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
+			{
+				//viewport variable
+				if(window_)
+					nsp = window_->current_viewportp();
+				goto err;
+			}
+			else
+#endif /* FIM_WINDOWS */
+			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
+			{
+				//image variable
+				nsp = (Image*) browser_.c_image(); /* FIXME: porcata assurda */
+				goto err;
+			}
+			else
+			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
+			{
+				//browser variable
+				nsp = & browser_;
+				goto err;
+			}
+			else
+			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
+			{
+				nsp = (Namespace*) this;
+				goto err;
+			}
+			else
+			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
+			{
+				//invalid namespace
+				goto err;
+			}
+			}
+			catch(FimException e){}
+		}
+#endif /* FIM_NAMESPACES */
+		nsp = this;
+err:
+		return nsp;
+	}
+
+	const Namespace * CommandConsole::c_rns(const fim::string varname)const
+	{
+		const Namespace * nsp = NULL;
+#ifdef FIM_NAMESPACES
+		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
+		{
+			try
+			{
+			//a specific namespace was selected!
+			fim_char_t ns = varname[0];
+			fim::string id = varname.c_str()+2;
+
+			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
+#ifdef FIM_WINDOWS
+			{
+				//window variable
+				nsp = window_;
+				goto err;
+			}
+			else
+			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
+			{
+				//viewport variable
+				if(window_)
+					nsp = window_->current_viewportp();
+				goto err;
+			}
+			else
+#endif /* FIM_WINDOWS */
+			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
+			{
+				//image variable
+				nsp = (Image*) browser_.c_image(); /* FIXME: porcata assurda */
+				goto err;
+			}
+			else
+			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
+			{
+				//browser variable
+				nsp = & browser_;
+				goto err;
+			}
+			else
+			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
+			{
+				nsp = (Namespace*) this;
+				goto err;
+			}
+			else
+			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
+			{
+				//invalid namespace
+				goto err;
+			}
+			}
+			catch(FimException e){}
+		}
+#endif /* FIM_NAMESPACES */
+		nsp = this;
+err:
+		return nsp;
+	}
+
 #if FIM_WANT_LONG_INT
 	fim_int CommandConsole::setVariable(const fim::string& varname,    int value)
 	{
@@ -32,611 +174,86 @@ namespace fim
 
 	fim_int CommandConsole::setVariable(const fim::string& varname,fim_int value)
 	{
-		/*
-		 * an internal function to set a user variable
-		 */
 		fim_int retval = 0;
+		Namespace *nsp = rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			try
-			{
-			//a specific namespace was selected!
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-#ifdef FIM_WINDOWS
-			{
-				//window variable
-				if(window_)
-					retval = window_->setVariable(id,value);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->setVariable(id,value);
-				goto err;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				retval =	
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->setVariable(id,value):
-					0;
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				retval = browser_.setVariable(id,value);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				retval = variables_[id].setInt(value);
-				goto err;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				goto err;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-		retval = variables_[varname].setInt(value);
+		if(nsp)
+			retval = nsp->setVariable(rnid(varname),value);
 err:
 		return retval;
 	}
 
 	fim_float_t CommandConsole::setVariable(const fim::string& varname,fim_float_t value)
 	{
-		/*
-		 * an internal function to set a user variable
-		 */
-//		cout << "setVariable " << variables_[varname].setFloat(value) << "\n"; 
-		fim_float_t retval = 0.0;
+		fim_float_t retval = FIM_CNS_EMPTY_FP_VAL;
+		Namespace *nsp = rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			try
-			{
-			//a specific namespace was selected!
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					retval = window_->setVariable(id,value);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->setVariable(id,value);
-				goto err;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				retval = browser_.c_image()?
-					( (Image*) (browser_.c_image()))->setVariable(id,value):
-					0;
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				retval = browser_.setVariable(id,value);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				retval = variables_[id].setFloat(value);
-				goto err;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				goto err;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-		retval = variables_[varname].setFloat(value);
+		if(nsp)
+			retval = nsp->setVariable(rnid(varname),value);
 err:
 		return retval;
 	}
 
 	fim_int CommandConsole::setVariable(const fim::string& varname,const fim_char_t*value)
 	{
-		/*
-		 * an internal function to set a user variable
-		 */
-		fim::string s(value);
 		fim_int retval = 0;
+		Namespace *nsp = rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			//a specific namespace was selected!
-			try
-			{
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					retval = window_->setVariable(id,s.c_str());
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->setVariable(id,s.c_str());
-				goto err;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				retval = 	
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->setVariable(id,s.c_str()):
-					0;
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				retval = browser_.setVariable(id,s.c_str());
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				retval = (fim_int)(variables_[id].setString(s));
-				goto err;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				goto err;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_WINDOWS */
-		retval = (fim_int)(variables_[varname].setString(s));
+		if(nsp)
+			retval = nsp->setVariable(rnid(varname),value);
 err:
 		return retval;
 	}
 
 	Var CommandConsole::setVariable(const fim::string varname,const Var&value)
 	{
-		/*
-		 * an internal function to set a user variable
-		 */
-//		std::cout << "CCSV\n";
 		Var retval = 0;
+		Namespace *nsp = rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			//a specific namespace was selected!
-			try
-			{
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					retval = window_->setVariable(id,value);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->setVariable(id,value);
-				goto err;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				if(browser_.c_image())
-				DBG(varname<<" : "<<value.getType()<<"\n");
-				retval = 	
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->setVariable(id,value):
-					Var(0);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				retval = browser_.setVariable(id,value);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				variables_[id]=value;
-				variables_[id].set(value);
-				goto err;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				goto err;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-		variables_[varname]=value;	/* FIXME : this does not work */
-		variables_[varname].set(value);	/* FIXME : this works */
-		//std::cout <<"CCGS["<<varname<<"]"<<variables_[varname].getString()<<"\n";
-		//std::cout <<"CCGS["<<varname<<"]"<<value.getString()<<"\n";
+		if(nsp)
+			retval = nsp->setVariable(rnid(varname),value);
 err:
-		retval = value;
 		return retval;
 	}
 
 	fim_int CommandConsole::getIntVariable(const fim::string &varname)const
 	{
 		fim_int retval = 0;
+		const Namespace *nsp = c_rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			try
-			{
-			//a specific namespace was selected!
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					retval = window_->getIntVariable(id);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->getIntVariable(id);
-				goto err;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				retval = 	
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->getIntVariable(id):
-					0;
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				retval = browser_.getIntVariable(id);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				variables_t::const_iterator vi=variables_.find(id);
-				if(vi!=variables_.end())
-					retval = vi->second.getInt();
-				goto err;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				goto err;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-		// this scope was selected
-		/*
-		 * the variable name supplied is used as a key to the variables hash
-		 *
-		 * FIXME : now the random stuff is done at interpreter level.
-		 * AND IT SHOULD NOT BE USED INTERNALY BY FIM!
-		 *
-		 * BEWARE!
-		 * */
-		{
-			variables_t::const_iterator vi=variables_.find(varname);
-			if(vi!=variables_.end())
-				retval = vi->second.getInt();
-		}
+		if(nsp)
+			retval = nsp->getIntVariable(rnid(varname));
 err:
 		return retval;
-		//return ((c_variables_t)(variables_))[varname].getInt();
 	}
 
 	fim_float_t CommandConsole::getFloatVariable(const fim::string &varname)const
 	{
-		/*
-		 * the variable name supplied is used as a key to the variables hash
-		 * */
-//		cout << "getVariable " << varname  << " : " << variables_[varname].getFloat()<< "\n";
-//		cout << "getVariable " << varname  << ", type : " << variables_[varname].getType()<< "\n";
-		fim_float_t retval=FIM_CNS_EMPTY_FP_VAL;
+		fim_float_t retval = FIM_CNS_EMPTY_FP_VAL;
+		const Namespace *nsp = c_rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			try{	
-			//a specific namespace was selected!
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					retval = window_->getFloatVariable(id);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->getFloatVariable(id);
-				goto err;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				retval = 	
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->getFloatVariable(id):
-					FIM_CNS_EMPTY_FP_VAL;
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				retval = browser_.getFloatVariable(id);
-				goto err;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				variables_t::const_iterator vi=variables_.find(id);
-				if(vi!=variables_.end())
-					retval = vi->second.getFloat();
-				else
-			       		retval = FIM_CNS_EMPTY_FP_VAL;
-				goto err;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				retval = FIM_CNS_EMPTY_FP_VAL;
-				goto err;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-		{
-			variables_t::const_iterator vi=variables_.find(varname);
-			if(vi!=variables_.end())
-				retval = vi->second.getFloat();
-			else
-			       	retval = FIM_CNS_EMPTY_FP_VAL;
-		}
-err:
+		if(nsp)
+			retval = nsp->getFloatVariable(rnid(varname));
 		return retval;
-//		return variables_[varname].getFloat();
 	}
 
 	fim::string CommandConsole::getStringVariable(const fim::string &varname)const
 	{
-		/*
-		 * the variable name supplied is used as a key to the variables hash
-		 * */
 		fim::string retval = FIM_CNS_EMPTY_RESULT;
+		const Namespace *nsp = c_rns(varname);
 
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			try
-			{
-			//a specific namespace was selected!
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					retval = window_->getStringVariable(id);
-				goto ret;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					retval = window_->current_viewportp()->getStringVariable(id);
-				goto ret;
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				retval = 	
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->getStringVariable(id):
-					FIM_CNS_EMPTY_RESULT;
-				goto ret;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-//				std::cout << "brbbbr\n";
-				retval = browser_.getStringVariable(id);
-				goto ret;
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				variables_t::const_iterator vi=variables_.find(id);
-				if(vi!=variables_.end()) 
-				retval = vi->second.getString();
-				goto ret;
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				retval = FIM_CNS_EMPTY_RESULT;
-				goto ret;
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-//		return variables_[varname].getString();
-		{
-			variables_t::const_iterator vi=variables_.find(varname);
-			if(vi!=variables_.end()) 
-			retval = vi->second.getString();
-		}
-ret:
+		if(nsp)
+			retval = nsp->getStringVariable(rnid(varname));
 		return retval;
 	}
 
-		Var CommandConsole::getVariable(const fim::string &varname)const
-		{
-		/*
-		 * the variable name supplied is used as a key to the variables hash
-		 * */
-#ifdef FIM_NAMESPACES
-		if( varname[1]==FIM_SYM_NAMESPACE_SEP )
-		{
-			try{	
-			//a specific namespace was selected!
-			fim_char_t ns = varname[0];
-			fim::string id=varname.c_str()+2;
-#ifdef FIM_WINDOWS
-			if( ns == FIM_SYM_NAMESPACE_WINDOW_CHAR )
-			{
-				//window variable
-				if(window_)
-					return window_->getVariable(id);
-				else
-					return Var();
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_VIEWPORT_CHAR )
-			{
-				//viewport variable
-				if(window_ && window_->current_viewportp())
-					return window_->current_viewportp()->getVariable(id);
-				else
-					return Var();
-			}
-			else
-#endif /* FIM_WINDOWS */
-			if( ns == FIM_SYM_NAMESPACE_IMAGE_CHAR )
-			{
-				//image variable
-				return
-					browser_.c_image()?
-					( (Image*) (browser_.c_image()))->getVariable(id):
-					Var();
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_BROWSER_CHAR )
-			{
-				//browser variable
-				return browser_.getVariable(id);
-			}
-			else
-			if( ns == FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				variables_t::const_iterator vi=variables_.find(id);
+	Var CommandConsole::getVariable(const fim::string &varname)const
+	{
+		const Namespace *nsp = c_rns(varname);
 
-				if(vi!=variables_.end())
-					return vi->second;
-				else
-			       		return Var();
-			}
-			else
-			if( ns != FIM_SYM_NAMESPACE_GLOBAL_CHAR )
-			{
-				//invalid namespace
-				return Var();
-			}
-			}
-			catch(FimException e){}
-		}
-#endif /* FIM_NAMESPACES */
-		variables_t::const_iterator vi=variables_.find(varname);
-
-		if(vi!=variables_.end())
-			return vi->second;
+		if(nsp)
+			return nsp->getVariable(rnid(varname));
 		else
-		       	return Var();
-		}
-
+			return Var();
+	}
 }
