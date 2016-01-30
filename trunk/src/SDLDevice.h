295114864a8a23a9d304f63501402aedd6433a88
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2016-01-30 12:18:40 +0000

    Bugfix: in r950 was using getIntVariable instead of getGlobalIntVariable. This used to break post-scroll console refresh.

--- trunk/src/SDLDevice.h
+++ trunk/src/SDLDevice.h
@@ -2,7 +2,7 @@
 /*
  SDLDevice.h : sdllib device Fim driver header file
 
- (c) 2008-2013 Michele Martone
+ (c) 2008-2015 Michele Martone
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
