82adfb8a7d40d4749d6c60d2a02d76d4561c8268
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2017-07-15 09:38:14 +0000

    Got rid of obsolete Browser::fcmd_reduce and Browser::fcmd_magnify.

--- trunk/src/Browser.h
+++ trunk/src/Browser.h
@@ -292,10 +292,6 @@ class Browser
 	fim_cxr fcmd_scrollforward(const args_t& args);
 	fim_cxr fcmd_scale(const args_t& args);
 #if FIM_WANT_FAT_BROWSER
-	fim_cxr fcmd_reduce(const args_t& args);
-	fim_cxr fcmd_magnify(const args_t& args);
-	fim_cxr fcmd_next(const args_t& args);
-	fim_cxr fcmd_prev(const args_t& args);
 	//fim_cxr fcmd_no_image(const args_t& args);
 #endif /* FIM_WANT_FAT_BROWSER */
 	fim_cxr fcmd_rotate(const args_t& args);
