efb0eda83458fc4003810321027daefe314c5ac2
dezperado <dezperado@25f1f456-beb2-4e79-b479-cd4fb8bdc4d3>
2023-03-25 21:05:48 +0000

    move .fim_history-related test to make tests

--- scripts/maintenance/live-test.sh
+++ scripts/maintenance/live-test.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-# A simplistic and incomplete test to verify if fim honours its 'live' configuration. 
-FIM=src/fim
-FH=.fim_history
-rm $FH
-export HOME=`pwd` 
-#$FIM --help
-$FIM media/* -c quit
-#ls -ltr $FH
-test ! -f .fim_history
-return $?
+# obsolete; waiting for a better use
+true
